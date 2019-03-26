e9a319925dd1312703b6a56204f07b0b64580267
Jakers <jakers@Jakerss-MacBook-Pro.local>
2019-03-26 18:23:39 -0400

    overflow fix

--- insert.sql
+++ insert.sql
@@ -3,6 +3,9 @@
    
    
    
+    INSERT INTO oms_by_town_live (`out`, town, `timestamp`, off, tk, etr) VALUES (10,"WESTFIELD","2019-03-14 14:02:37","2019-03-14 10:18:49","17-2-3C","2019-03-14 18:42:00"),
+    INSERT INTO oms_by_town_live (`out`, town, `timestamp`, off, tk, etr) VALUES (10,"STOWE","2019-03-14 14:02:37","2019-03-14 10:18:49","17-2-3C","2019-03-14 18:42:00"),
+    INSERT INTO oms_by_town_live (`out`, town, `timestamp`, off, tk, etr) VALUES (10,"ALBANY","2019-03-14 14:02:37","2019-03-14 10:18:49","17-2-3C","2019-03-14 18:42:00"),
     INSERT INTO oms_by_town_live (`out`, town, `timestamp`, off, tk, etr) VALUES (10,"WESTFIELD","2019-03-14 14:02:37","2019-03-14 10:18:49","17-2-3C","2019-03-14 18:42:00")
 
 
